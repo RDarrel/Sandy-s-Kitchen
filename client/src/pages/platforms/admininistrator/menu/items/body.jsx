6fac3bfc04656ab044ea00aa5609695a46623c78
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-04-15 21:55:04 +0800

    update

--- client/src/pages/platforms/admininistrator/menu/items/body.jsx
+++ client/src/pages/platforms/admininistrator/menu/items/body.jsx
@@ -20,21 +20,19 @@ const ItemSkeleton = () => (
     <div className="relative h-52 overflow-hidden">
       <Skeleton className="h-full w-full rounded-none" />
       <div className="absolute left-4 top-4 flex gap-2">
-        <Skeleton className="h-7 w-20 rounded-full bg-white/70" />
-        <Skeleton className="h-7 w-16 rounded-full bg-white/40" />
+        <Skeleton className="h-7 w-20 rounded-full" />
+      </div>
+      <div className="absolute right-3 top-3 flex items-start gap-1.5">
+        <Skeleton className="h-7 w-24 rounded-full" />
+        <Skeleton className="h-[30px] w-[30px] rounded-full" />
       </div>
       <div className="absolute bottom-4 left-4 right-4 flex items-end justify-between gap-3">
-        <Skeleton className="h-7 w-36 bg-white/70" />
-        <Skeleton className="h-10 w-20 rounded-xl bg-white/80" />
+        <Skeleton className="h-7 w-36" />
+        <Skeleton className="h-10 w-20 rounded-xl" />
       </div>
     </div>
 
     <div className="space-y-4 p-4">
-      <div className="flex items-center justify-between gap-3">
-        <Skeleton className="h-7 w-24 rounded-full" />
-        <Skeleton className="h-8 w-28 rounded-full" />
-      </div>
-
       <div className="space-y-2">
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-4/5" />
@@ -48,11 +46,6 @@ const ItemSkeleton = () => (
       <div className="space-y-2">
         <Skeleton className="h-3.5 w-full" />
         <Skeleton className="h-3.5 w-3/4" />
-      </div>
-
-      <div className="flex gap-2 pt-2">
-        <Skeleton className="h-11 flex-1 rounded-xl" />
-        <Skeleton className="h-11 flex-1 rounded-xl" />
       </div>
     </div>
   </div>
