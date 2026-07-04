7f9aaa3bc6af4b80ddd706bfe87ea5e83d747470
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-07-04 13:39:03 +0800

    cleaning

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,12 +1,14 @@
 import About from "./about";
 import Hero from "./hero";
 import Offers from "./offers";
+import Footer from "./footer";
 const HomePage = () => {
   return (
     <>
       <Hero />
       <About />
       <Offers />
+      <Footer />
     </>
   );
 };
