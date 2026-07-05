5b5f625098683d45b8544797abbcbd716893895f
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-07-05 18:14:45 +0800

    update

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -32,26 +32,6 @@ const SectionDivider = () => (
           className="home-divider__curve home-divider__curve--soft"
           d="M736 26C770 28 824 35 914 29C1010 22 1084 36 1182 29C1272 23 1348 35 1440 29"
         />
-        <path
-          className="home-divider__spark home-divider__spark--left home-divider__spark--top"
-          pathLength="1"
-          d="M704 24C670 22 616 15 526 21C430 28 356 14 258 21C168 27 92 15 0 21"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--left home-divider__spark--bottom"
-          pathLength="1"
-          d="M704 26C670 28 616 35 526 29C430 22 356 36 258 29C168 23 92 35 0 29"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--right home-divider__spark--top"
-          pathLength="1"
-          d="M736 24C770 22 824 15 914 21C1010 28 1084 14 1182 21C1272 27 1348 15 1440 21"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--right home-divider__spark--bottom"
-          pathLength="1"
-          d="M736 26C770 28 824 35 914 29C1010 22 1084 36 1182 29C1272 23 1348 35 1440 29"
-        />
       </g>
       <g className="home-divider__path-set home-divider__path-set--mobile">
         <path
@@ -68,26 +48,6 @@ const SectionDivider = () => (
         />
         <path
           className="home-divider__curve home-divider__curve--soft"
-          d="M780 26C802 28 830 35 914 29C1010 22 1084 36 1182 29C1272 23 1348 35 1440 29"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--left home-divider__spark--top"
-          pathLength="1"
-          d="M660 24C638 22 610 15 526 21C430 28 356 14 258 21C168 27 92 15 0 21"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--left home-divider__spark--bottom"
-          pathLength="1"
-          d="M660 26C638 28 610 35 526 29C430 22 356 36 258 29C168 23 92 35 0 29"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--right home-divider__spark--top"
-          pathLength="1"
-          d="M780 24C802 22 830 15 914 21C1010 28 1084 14 1182 21C1272 27 1348 15 1440 21"
-        />
-        <path
-          className="home-divider__spark home-divider__spark--right home-divider__spark--bottom"
-          pathLength="1"
           d="M780 26C802 28 830 35 914 29C1010 22 1084 36 1182 29C1272 23 1348 35 1440 29"
         />
       </g>
