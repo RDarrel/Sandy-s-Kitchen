fc2d75a630aec3101c4cbae5d94b9b93bc8579f4
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-07-05 15:00:09 +0800

    update

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -5,17 +5,44 @@ import Footer from "./footer";
 import WhyChoose from "./whyChoose";
 import Moments from "./moments";
 import FAQs from "./FAQs";
+import "./style.css";
+
+const SectionDivider = () => (
+  <div className="home-divider" aria-hidden="true">
+    <svg
+      className="home-divider__wave"
+      viewBox="0 0 1440 48"
+      preserveAspectRatio="none"
+      focusable="false"
+    >
+      <path
+        className="home-divider__curve"
+        d="M0 24C190 17 296 31 432 24C568 17 640 18 720 24C812 31 914 31 1040 24C1182 16 1284 18 1440 24"
+      />
+    </svg>
+    <div className="home-divider__ornament">
+      <span />
+      <span />
+      <span />
+    </div>
+  </div>
+);
+
 const HomePage = () => {
   return (
-    <>
+    <main className="home-page">
       <Hero />
       <About />
+      <SectionDivider />
       <Offers />
+      <SectionDivider />
       <WhyChoose />
+      <SectionDivider />
       <FAQs />
+      <SectionDivider />
       <Moments />
       <Footer />
-    </>
+    </main>
   );
 };
 
