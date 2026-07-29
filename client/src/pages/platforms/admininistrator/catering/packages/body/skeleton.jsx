73347b7cd12465b25dfe889eac571ff1f1305afd
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-07-30 03:02:12 +0800

    create a skeleton for package list

--- client/src/pages/platforms/admininistrator/catering/packages/body/skeleton.jsx
+++ client/src/pages/platforms/admininistrator/catering/packages/body/skeleton.jsx
@@ -6,19 +6,19 @@ const PackageSkeleton = () => {
       <div>
         <Skeleton className={"h-full w-full "} />
       </div>
-      <div className="p-3 flex flex-col gap-3 min-w-0">
+      <div className="p-3 flex flex-col gap-2 min-w-0">
         <div className="grid grid-cols-[20rem_1fr] gap-5">
           <div className="flex flex-col gap-2">
-            <Skeleton className={"h-7 w-40"} />
-            <Skeleton className={"h-9 w-full"} />
+            <Skeleton className={"h-6 w-40"} />
+            <Skeleton className={"h-7 w-full"} />
           </div>
           <div className="grid grid-cols-[1fr_auto]">
             <div className="flex flex-col gap-2">
-              <Skeleton className="h-7 w-36" />
-              <Skeleton className="h-5 w-36" />
+              <Skeleton className="h-6 w-36" />
+              <Skeleton className="h-4 w-36" />
             </div>
 
-            <Skeleton className="h-10 w-4" />
+            <Skeleton className="h-8 w-4" />
           </div>
         </div>
 
@@ -32,6 +32,19 @@ const PackageSkeleton = () => {
             ))}
           </div>
           <hr />
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          {new Array(6).fill("").map((_, idx) => {
+            const width = 100 - ((idx * 17) % 40);
+
+            return (
+              <Skeleton
+                key={idx}
+                className="h-4"
+                style={{ width: `${width}%` }}
+              />
+            );
+          })}
         </div>
         <Skeleton className={"w-full h-8 mt-auto"} />
       </div>
