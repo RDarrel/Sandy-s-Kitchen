343527cf2181c2480f09e77243707ed031d27950
RDarrel <116413862+RDarrel@users.noreply.github.com>
2026-08-11 02:07:40 +0800

    refactor(customer-package)

--- client/src/pages/platforms/customer/catering/index.jsx
+++ client/src/pages/platforms/customer/catering/index.jsx
@@ -1,12 +1,7 @@
 import Catering from "@/pages/website/catering";
 
 const CateringParent = () => {
-  return (
-    <div className="my-3 md:my-0">
-      <div className="text-center">test</div>
-      <Catering />
-    </div>
-  );
+  return <Catering />;
 };
 
 export default CateringParent;
